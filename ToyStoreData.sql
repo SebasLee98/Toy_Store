439015c447a18f24ba9f35b55ba1f08c0615c079
SebasLee98 <sebastianhclee@gmail.com>
2026-07-27 20:23:47 -0700

    added sequence column and formatting

--- ToyStoreData.sql
+++ ToyStoreData.sql
@@ -74,7 +74,11 @@ WITH cust_specs AS (
     FROM general_info GROUP BY customerNumber
 )
 -- From that broader query, show final result of difference between amount owed and paid
-SELECT E.customerName, C.customerNumber, C.Expected_Payment, sum(D.amount) AS Actual_Payment, C.Expected_Payment - sum(D.amount) AS Remaining_Balance, round((sum(D.amount) / C.Expected_Payment) * 100, 2) AS Percent_paid
+SELECT 
+ROW_NUMBER () OVER (ORDER BY (sum(D.amount) / C.Expected_Payment)) AS Table_Number, 
+E.customerName, C.customerNumber, C.Expected_Payment, sum(D.amount) AS Actual_Payment, 
+C.Expected_Payment - sum(D.amount) AS Remaining_Balance, 
+round((sum(D.amount) / C.Expected_Payment) * 100, 2) AS Percent_paid
 FROM cust_specs C 
 LEFT JOIN payments D ON C.customerNumber = D.customerNumber
 LEFT JOIN customers E ON D.customerNumber = E.customerNumber 
